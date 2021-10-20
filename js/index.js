933d86a749b3b8b27c756a5de743ff1c274f1ddc
SeanRavenhill <seanravenhill@gmail.com>
2021-10-20 16:21:26 +0200

    Javascript logic to handle changing of time reporst complete

--- js/index.js
+++ js/index.js
@@ -1,20 +1,191 @@
-async function getData() {
-    let response = await fetch("./data/data.json");
-    let data = await response.json();
-    document.getElementById("work-current").textContent = data[0].timeframes.weekly.current + "hrs";
-    document.getElementById("work-previous").textContent = "Last week - " + data[0].timeframes.weekly.previous + "hrs";
-    document.getElementById("play-current").textContent = data[1].timeframes.weekly.current + "hrs";
-    document.getElementById("play-previous").textContent = "Last week - " + data[1].timeframes.weekly.previous + "hrs";
-    document.getElementById("study-current").textContent = data[2].timeframes.weekly.current + "hrs";
-    document.getElementById("study-previous").textContent = "Last week - " + data[2].timeframes.weekly.previous + "hrs";
-    document.getElementById("exercise-current").textContent = data[3].timeframes.weekly.current + "hrs";
-    document.getElementById("exercise-previous").textContent = "Last week - " + data[3].timeframes.weekly.previous + "hrs";
-    document.getElementById("social-current").textContent = data[4].timeframes.weekly.current + "hrs";
-    document.getElementById("social-previous").textContent = "Last week - " + data[4].timeframes.weekly.previous + "hrs";
-    document.getElementById("self-care-current").textContent = data[5].timeframes.weekly.current + "hrs";
-    document.getElementById("self-care-previous").textContent = "Last week - " + data[5].timeframes.weekly.previous + "hrs";
-}
+document.getElementById("btn-daily").addEventListener("click", function () {
+	async function getData() {
+		let response = await fetch("./data/data.json");
+		let data = await response.json();
 
-getData()
+        console.log(document.getElementById("btn-daily").className)
 
+		document.getElementById("work-current").textContent =
+			data[0].timeframes.daily.current === 1
+				? data[0].timeframes.daily.current + "hr"
+				: data[0].timeframes.daily.current + "hrs";
+		document.getElementById("work-previous").textContent =
+			data[0].timeframes.daily.previous === 1
+				? "Yesterday - " + data[0].timeframes.daily.previous + "hr"
+				: "Yesterday - " + data[0].timeframes.daily.previous + "hrs";
 
+		document.getElementById("play-current").textContent =
+			data[1].timeframes.daily.current === 1
+				? data[1].timeframes.daily.current + "hr"
+				: data[1].timeframes.daily.current + "hrs";
+		document.getElementById("play-previous").textContent =
+			data[1].timeframes.daily.previous === 1
+				? "Yesterday - " + data[1].timeframes.daily.previous + "hr"
+				: "Yesterday - " + data[1].timeframes.daily.previous + "hrs";
+
+		document.getElementById("study-current").textContent =
+			data[2].timeframes.daily.current === 1
+				? data[2].timeframes.daily.current + "hr"
+				: data[2].timeframes.daily.current + "hrs";
+		document.getElementById("study-previous").textContent =
+			data[2].timeframes.daily.previous === 1
+				? "Yesterday - " + data[2].timeframes.daily.previous + "hr"
+				: "Yesterday - " + data[2].timeframes.daily.previous + "hrs";
+
+		document.getElementById("exercise-current").textContent =
+			data[3].timeframes.daily.current === 1
+				? data[3].timeframes.daily.current + "hr"
+				: data[3].timeframes.daily.current + "hrs";
+		document.getElementById("exercise-previous").textContent =
+			data[3].timeframes.daily.previous === 1
+				? "Yesterday - " + data[3].timeframes.daily.previous + "hr"
+				: "Yesterday - " + data[3].timeframes.daily.previous + "hrs";
+
+		document.getElementById("social-current").textContent =
+			data[4].timeframes.daily.current === 1
+				? data[4].timeframes.daily.current + "hr"
+				: data[4].timeframes.daily.current + "hrs";
+		document.getElementById("social-previous").textContent =
+			data[4].timeframes.daily.previous === 1
+				? "Yesterday - " + data[4].timeframes.daily.previous + "hr"
+				: "Yesterday - " + data[4].timeframes.daily.previous + "hrs";
+
+		document.getElementById("self-care-current").textContent =
+			data[5].timeframes.daily.current === 1
+				? data[5].timeframes.daily.current + "hr"
+				: data[5].timeframes.daily.current + "hrs";
+		document.getElementById("self-care-previous").textContent =
+			data[5].timeframes.daily.previous === 1
+				? "Yesterday - " + data[5].timeframes.daily.previous + "hr"
+				: "Yesterday - " + data[5].timeframes.daily.previous + "hrs";
+	}
+	getData();
+});
+
+document.getElementById("btn-weekly").addEventListener("click", function () {
+	async function getData() {
+		let response = await fetch("./data/data.json");
+		let data = await response.json();
+
+        console.log(document.getElementById("btn-weekly").className)
+
+		document.getElementById("work-current").textContent =
+			data[0].timeframes.weekly.current === 1
+				? data[0].timeframes.weekly.current + "hr"
+				: data[0].timeframes.weekly.current + "hrs";
+		document.getElementById("work-previous").textContent =
+			data[0].timeframes.weekly.previous === 1
+				? "Last Week - " + data[0].timeframes.weekly.previous + "hr"
+				: "Last Week - " + data[0].timeframes.weekly.previous + "hrs";
+
+		document.getElementById("play-current").textContent =
+			data[1].timeframes.weekly.current === 1
+				? data[1].timeframes.weekly.current + "hr"
+				: data[1].timeframes.weekly.current + "hrs";
+		document.getElementById("play-previous").textContent =
+			data[1].timeframes.weekly.previous === 1
+				? "Last Week - " + data[1].timeframes.weekly.previous + "hr"
+				: "Last Week - " + data[1].timeframes.weekly.previous + "hrs";
+
+		document.getElementById("study-current").textContent =
+			data[2].timeframes.weekly.current === 1
+				? data[2].timeframes.weekly.current + "hr"
+				: data[2].timeframes.weekly.current + "hrs";
+		document.getElementById("study-previous").textContent =
+			data[2].timeframes.daily.previous === 1
+				? "Last Week - " + data[2].timeframes.weekly.previous + "hr"
+				: "Last Week - " + data[2].timeframes.weekly.previous + "hrs";
+
+		document.getElementById("exercise-current").textContent =
+			data[3].timeframes.weekly.current === 1
+				? data[3].timeframes.weekly.current + "hr"
+				: data[3].timeframes.weekly.current + "hrs";
+		document.getElementById("exercise-previous").textContent =
+			data[3].timeframes.weekly.previous === 1
+				? "Last Week - " + data[3].timeframes.weekly.previous + "hr"
+				: "Last Week - " + data[3].timeframes.weekly.previous + "hrs";
+
+		document.getElementById("social-current").textContent =
+			data[4].timeframes.weekly.current === 1
+				? data[4].timeframes.weekly.current + "hr"
+				: data[4].timeframes.weekly.current + "hrs";
+		document.getElementById("social-previous").textContent =
+			data[4].timeframes.weekly.previous === 1
+				? "Last Week - " + data[4].timeframes.weekly.previous + "hr"
+				: "Last Week - " + data[4].timeframes.weekly.previous + "hrs";
+
+		document.getElementById("self-care-current").textContent =
+			data[5].timeframes.weekly.current === 1
+				? data[5].timeframes.weekly.current + "hr"
+				: data[5].timeframes.weekly.current + "hrs";
+		document.getElementById("self-care-previous").textContent =
+			data[5].timeframes.weekly.previous === 1
+				? "Last Week - " + data[5].timeframes.weekly.previous + "hr"
+				: "Last Week - " + data[5].timeframes.weekly.previous + "hrs";
+	}
+	getData();
+});
+
+document.getElementById("btn-monthly").addEventListener("click", function () {
+	async function getData() {
+		let response = await fetch("./data/data.json");
+		let data = await response.json();
+
+        console.log(document.getElementById("btn-monthly").className)
+
+		document.getElementById("work-current").textContent =
+			data[0].timeframes.monthly.current === 1
+				? data[0].timeframes.monthly.current + "hr"
+				: data[0].timeframes.monthly.current + "hrs";
+		document.getElementById("work-previous").textContent =
+			data[0].timeframes.monthly.previous === 1
+				? "Last Month - " + data[0].timeframes.monthly.previous + "hr"
+				: "Last Month - " + data[0].timeframes.monthly.previous + "hrs";
+
+		document.getElementById("play-current").textContent =
+			data[1].timeframes.monthly.current === 1
+				? data[1].timeframes.monthly.current + "hr"
+				: data[1].timeframes.monthly.current + "hrs";
+		document.getElementById("play-previous").textContent =
+			data[1].timeframes.monthly.previous === 1
+				? "Last Month - " + data[1].timeframes.monthly.previous + "hr"
+				: "Last Month - " + data[1].timeframes.monthly.previous + "hrs";
+
+		document.getElementById("study-current").textContent =
+			data[2].timeframes.monthly.current === 1
+				? data[2].timeframes.monthly.current + "hr"
+				: data[2].timeframes.monthly.current + "hrs";
+		document.getElementById("study-previous").textContent =
+			data[2].timeframes.daily.previous === 1
+				? "Last Month - " + data[2].timeframes.monthly.previous + "hr"
+				: "Last Month - " + data[2].timeframes.monthly.previous + "hrs";
+
+		document.getElementById("exercise-current").textContent =
+			data[3].timeframes.monthly.current === 1
+				? data[3].timeframes.monthly.current + "hr"
+				: data[3].timeframes.monthly.current + "hrs";
+		document.getElementById("exercise-previous").textContent =
+			data[3].timeframes.monthly.previous === 1
+				? "Last Month - " + data[3].timeframes.monthly.previous + "hr"
+				: "Last Month - " + data[3].timeframes.monthly.previous + "hrs";
+
+		document.getElementById("social-current").textContent =
+			data[4].timeframes.monthly.current === 1
+				? data[4].timeframes.monthly.current + "hr"
+				: data[4].timeframes.monthly.current + "hrs";
+		document.getElementById("social-previous").textContent =
+			data[4].timeframes.monthly.previous === 1
+				? "Last Month - " + data[4].timeframes.monthly.previous + "hr"
+				: "Last Month - " + data[4].timeframes.monthly.previous + "hrs";
+
+		document.getElementById("self-care-current").textContent =
+			data[5].timeframes.monthly.current === 1
+				? data[5].timeframes.monthly.current + "hr"
+				: data[5].timeframes.monthly.current + "hrs";
+		document.getElementById("self-care-previous").textContent =
+			data[5].timeframes.weekly.previous === 1
+				? "Last Month - " + data[5].timeframes.monthly.previous + "hr"
+				: "Last Month - " + data[5].timeframes.monthly.previous + "hrs";
+	}
+	getData();
+});
